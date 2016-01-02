9b21bae68d216540f0ea9296657a6e352bedfa5c
Steven Vitali <svitali@seas.upenn.edu>
2016-01-02 14:22:59 -0800

    Implement user signup, login, logout

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 var user = require('./controllers/user');
+var User = require('./models/user');
 
 var isLoggedIn = function(req) {
-  return !!(req.session && req.session.user);
+  return !!req.user;
 };
 
 // Middleware that redirects a route to '/'
@@ -17,12 +19,41 @@ var loggedIn = function(req, res, next) {
 
 module.exports = function(app) {
   app.get('/', function(req, res) {
+    console.log('rendering', req.user);
     res.render('index', {
-      user: req.session.user || null
+      user: req.user
     });
   });
 
-  app.post('/login', user.login);
-  app.post('/logout', user.logout);
-  app.post('/signup', user.signup);
+  app.get('/register', function(req, res) {
+    res.render('register', {});
+  });
+
+  app.post('/register', function(req, res) {
+    var account = new User({ email: req.body.username });
+    var password = req.body.password;
+    User.register(account, password, function(err, account) {
+      if (err) {
+        console.log(err);
+        return res.render('register', { account: account });
+      }
+      passport.authenticate('local')(req, res, function() {
+        res.redirect('/');
+      });
+    });
+  });
+
+  app.get('/login', function(req, res) {
+    res.render('login', { user: req.user });
+  });
+
+  app.post('/login', passport.authenticate('local'), function(req, res) {
+    console.log('authenticated...', req.user);
+    res.redirect('/');
+  });
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 };
