e8ae364cefa2ff11dd7eede445f9f1941a8d3c46
Steven Vitali <svitali@seas.upenn.edu>
2016-01-03 05:54:05 -0800

    Clean up white pages API

--- controllers/white-pages.js
+++ controllers/white-pages.js
@@ -1,24 +1,31 @@
 var _ = require('lodash');
 var request = require('request');
 
+var onErr = function(err, res) {
+  err && console.log(err);
+  res.status(500).send(err);
+};
+
 exports.identityCheck = function(req, res) {
-  var url = 'https://proapi.whitepages.com/2.1/person.json?api_key=0e3db2f4c8b06067fc10a70feb5aeb24';
+  var url = 'https://proapi.whitepages.com/2.1/person.json';
+  url += '?api_key=0e3db2f4c8b06067fc10a70feb5aeb24';
+
   _.each(req.body.address, function(value, key) {
     value && (url += '&' + key + '=' + value);
   });
+
   url += '&name=' + req.body.firstName + ' ' + req.body.lastName;
   url = url.split(' ').join('+').toLowerCase();
-  console.log('white pages request url', url);
-  var first = true;
+
   request({
     method: 'GET',
     url: url
-  }, function(err, response) {
-    var results = response.body;
-    results = JSON.parse(results || '{}').results || [];
+  },
+  function(err, response) {
+    if (err) return onErr(err, res);
+    results = JSON.parse(response.body || '{}').results || [];
     results = _.pluck(results, 'locations');
     results = _.flatten(results);
-    console.log('locations', results);
     res.send(results);
   });
 };
