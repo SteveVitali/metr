1f194a39bb3366f7ac94c748629ddb147c931797
Steven Vitali <svitali@seas.upenn.edu>
2016-01-03 04:32:57 -0800

    Update parking space occupy/leave endpoints

--- controllers/parking-space.js
+++ controllers/parking-space.js
@@ -1,41 +1,60 @@
 var request = require('request');
 var ParkingSpace = require('../models').ParkingSpace;
+var User = require('../models').User;
+
+var onErr = function(err, res) {
+  err && console.log(err);
+  res.status(500).send(err);
+};
 
 exports.occupy = function(req, res) {
-  // Triggered whenever a device sensor status goes from 0-1.
-  // 1. Corresponding parking space in the database is set to reserved
-  // from the given timestamp.
-  ParkingSpace.findById(req.params.id, function(err, space) {
+  var userId = req.user ? req.user._id : req.query.userId;
+  console.log('occupying', userId);
+  if (!userId) return onErr('Invalid user', res);
+  User.findById(userId, function(err, user) {
     if (err) return onErr(err, res);
-    // TODO handling for trying to reserve an already taken space?
-    space.isAvailable = false;
-    space.occupiedBy = req.user && req.user._id;
-    space.occupiedAt = new Date();
-    req.user.currentSpace = space._id;
-    space.save(function(err) {
-      if (err) return onErr(err, res);
-      req.user.save(function(err) {
+    // Triggered whenever a device sensor status goes from 0-1.
+    // 1. Corresponding parking space in the database is set to reserved
+    // from the given timestamp.
+    ParkingSpace.findById(req.params.id, function(err, space) {
+      if (err || !space) return onErr(err || 'Invalid space', res);
+      // TODO handling for trying to reserve an already taken space?
+      space.isAvailable = false;
+      space.occupiedBy = user && user._id;
+      space.occupiedAt = new Date();
+      user.currentSpace = space._id;
+      space.save(function(err) {
         if (err) return onErr(err, res);
-        res.send('Success');
+        user.save(function(err) {
+          if (err) return onErr(err, res);
+          res.send('Success');
+        });
       });
     });
   });
 };
 
 exports.leave = function(req, res) {
-  if (!req.user.currentSpace) return onErr('Not parked');
-  ParkingSpace.findById(req.user.currentSpace, function(err, space) {
-    req.user.currentSpace = undefined;
-    space.occupiedBy = undefined;
-    space.isAvailable = true;
-    var occupiedAt = new Date(space.occupiedAt);
-    var hours = Math.abs(now - occupiedAt) / 36e5;
-    space.save(function(err) {
-      if (err) return onErr(err, res);
-      req.user.save(function(err) {
+  var userId = req.user ? req.user._id : req.query.userId;
+  if (!userId) return onErr('Invalid user', res);
+  User.findById(userId, function(err, user) {
+    if (err || !user) return onErr(err, res);
+    if (!user.currentSpace) return onErr('Not parked');
+    ParkingSpace.findById(user.currentSpace, function(err, space) {
+      if (err) return onErr(err);
+      user.currentSpace = undefined;
+      space.occupiedBy = undefined;
+      space.isAvailable = true;
+      var occupiedAt = new Date(space.occupiedAt);
+      var now = new Date();
+      var hours = Math.abs(now - occupiedAt) / 36e5;
+      space.save(function(err) {
         if (err) return onErr(err, res);
-        res.send({
-          cost: hours * space.hourlyRate
+        user.save(function(err) {
+          if (err) return onErr(err, res);
+          res.send({
+            cost: hours * space.hourlyRate
+          });
         });
       });
     });
